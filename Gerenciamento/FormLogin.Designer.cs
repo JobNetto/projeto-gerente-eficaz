85e1039cf5285e8e18649ed6f64ad97f713a9a66
JobNetto <jobamnetto@gmail.com>
2018-10-27 01:07:57 -0300

    FIRST COMMIT

---
+++ Gerenciamento/FormLogin.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Gerenciamento
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogin));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(228, 149);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(377, 162);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(6, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(125, 150);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(267, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Entrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(152, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Senha";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(152, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Login";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(196, 59);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(146, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(196, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gerente Eficaz";
+            this.Load += new System.EventHandler(this.FormLogin_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Gerenciamento/TelaGeral.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Gerenciamento
+{
+    partial class TelaGeral
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaGeral));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.cadastrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viajemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.caminhãoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.motoristaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viajemToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.caminhãoToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.motoristaToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.relarorioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cadastrarToolStripMenuItem,
+            this.consultarToolStripMenuItem,
+            this.relarorioToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // cadastrarToolStripMenuItem
+            // 
+            this.cadastrarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viajemToolStripMenuItem,
+            this.caminhãoToolStripMenuItem,
+            this.motoristaToolStripMenuItem});
+            this.cadastrarToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("cadastrarToolStripMenuItem.Image")));
+            this.cadastrarToolStripMenuItem.Name = "cadastrarToolStripMenuItem";
+            this.cadastrarToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+            this.cadastrarToolStripMenuItem.Text = "Cadastrar";
+            // 
+            // viajemToolStripMenuItem
+            // 
+            this.viajemToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("viajemToolStripMenuItem.Image")));
+            this.viajemToolStripMenuItem.Name = "viajemToolStripMenuItem";
+            this.viajemToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.viajemToolStripMenuItem.Text = "Viajem";
+            // 
+            // caminhãoToolStripMenuItem
+            // 
+            this.caminhãoToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("caminhãoToolStripMenuItem.Image")));
+            this.caminhãoToolStripMenuItem.Name = "caminhãoToolStripMenuItem";
+            this.caminhãoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.caminhãoToolStripMenuItem.Text = "Caminhão";
+            // 
+            // motoristaToolStripMenuItem
+            // 
+            this.motoristaToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("motoristaToolStripMenuItem.Image")));
+            this.motoristaToolStripMenuItem.Name = "motoristaToolStripMenuItem";
+            this.motoristaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.motoristaToolStripMenuItem.Text = "Motorista";
+            this.motoristaToolStripMenuItem.Click += new System.EventHandler(this.motoristaToolStripMenuItem_Click);
+            // 
+            // consultarToolStripMenuItem
+            // 
+            this.consultarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viajemToolStripMenuItem1,
+            this.caminhãoToolStripMenuItem1,
+            this.motoristaToolStripMenuItem1});
+            this.consultarToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("consultarToolStripMenuItem.Image")));
+            this.consultarToolStripMenuItem.Name = "consultarToolStripMenuItem";
+            this.consultarToolStripMenuItem.Size = new System.Drawing.Size(86, 20);
+            this.consultarToolStripMenuItem.Text = "Consultar";
+            // 
+            // viajemToolStripMenuItem1
+            // 
+            this.viajemToolStripMenuItem1.Image = ((System.Drawing.Image)(resources.GetObject("viajemToolStripMenuItem1.Image")));
+            this.viajemToolStripMenuItem1.Name = "viajemToolStripMenuItem1";
+            this.viajemToolStripMenuItem1.Size = new System.Drawing.Size(129, 22);
+            this.viajemToolStripMenuItem1.Text = "Viajem";
+            // 
+            // caminhãoToolStripMenuItem1
+            // 
+            this.caminhãoToolStripMenuItem1.Image = ((System.Drawing.Image)(resources.GetObject("caminhãoToolStripMenuItem1.Image")));
+            this.caminhãoToolStripMenuItem1.Name = "caminhãoToolStripMenuItem1";
+            this.caminhãoToolStripMenuItem1.Size = new System.Drawing.Size(129, 22);
+            this.caminhãoToolStripMenuItem1.Text = "Caminhão";
+            // 
+            // motoristaToolStripMenuItem1
+            // 
+            this.motoristaToolStripMenuItem1.Image = ((System.Drawing.Image)(resources.GetObject("motoristaToolStripMenuItem1.Image")));
+            this.motoristaToolStripMenuItem1.Name = "motoristaToolStripMenuItem1";
+            this.motoristaToolStripMenuItem1.Size = new System.Drawing.Size(129, 22);
+            this.motoristaToolStripMenuItem1.Text = "Motorista";
+            // 
+            // relarorioToolStripMenuItem
+            // 
+            this.relarorioToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("relarorioToolStripMenuItem.Image")));
+            this.relarorioToolStripMenuItem.Name = "relarorioToolStripMenuItem";
+            this.relarorioToolStripMenuItem.Size = new System.Drawing.Size(82, 20);
+            this.relarorioToolStripMenuItem.Text = "Relarorio";
+            // 
+            // TelaGeral
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "TelaGeral";
+            this.Text = "Gerente Eficaz";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cadastrarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viajemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem caminhãoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem motoristaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viajemToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem caminhãoToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem motoristaToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem relarorioToolStripMenuItem;
+    }
+}
