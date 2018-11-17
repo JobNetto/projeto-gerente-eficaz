1ed73c879913bc2f01b9f974f676361d1720eaa5
JobNetto <jobamnetto@gmail.com>
2018-11-17 18:51:11 -0200

    caminhaoRep

--- Gerenciamento/model/Repository/CaminhaoRep.cs
+++ Gerenciamento/model/Repository/CaminhaoRep.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
+using Gerenciamento.models;
+using System.Data.SqlClient;
 
 namespace Gerenciamento.model.Repository
 {
@@ -27,12 +29,54 @@ namespace Gerenciamento.model.Repository
 
         public override DataSet GetAll(object objeto)
         {
-            throw new NotImplementedException();
+            string sql = "SELECT * FROM caminhao";
+            sql += " WHERE placa LIKE '" + objeto.ToString() + "%'";
+            SqlDataAdapter da = new SqlDataAdapter(sql, cn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
         }
 
         public override void Inserir(object objeto)
         {
-            throw new NotImplementedException();
+            if (objeto is Caminhao)
+            {
+                var caminhao = objeto as Caminhao;
+                string sql = "";
+                sql = $@"INSERT INTO [dbo].[Caminhao]
+           ([Marca]
+           ,[Placa]
+           ,[Codigo]
+           ,[Modelo]
+           ,[AnoFabricacao]
+           ,[AnoModelo]
+           ,[Chassi]
+           ,[CodRenavam]
+           ,[Cor]
+           ,[Capacidade]
+           ,[KmRodado])
+     VALUES
+           ('{caminhao.Marca}'
+           ,'{caminhao.Placa}'
+           ,'{caminhao.Codigo}'
+           ,'{caminhao.Modelo}'
+           ,'{caminhao.AnoFabricacao.ToString("yyyy-MM-dd")}'
+           ,'{caminhao.AnoModelo.ToString("yyyy-MM-dd")}'
+           ,{caminhao.Chassi}
+           ,{caminhao}
+           ,{caminhao}
+           ,{caminhao}
+           ,{caminhao}
+";
+
+                SqlCommand cd = new SqlCommand
+                {
+                    Connection = cn,
+                    CommandText = sql
+                };
+
+                cd.ExecuteNonQuery();
+            }
         }
     }
 }
